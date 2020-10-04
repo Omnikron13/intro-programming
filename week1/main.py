1e6479d4e4bb6acaf07dcb1ec6b5599957e5260d
Joey Sabey <joey.sabey@gmx.com>
2020-10-04 21:49:56 +0100

    Added week1 programming excercise

---
+++ week1/main.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct  1 18:00:34 2020
+
+@author: Joey
+"""
+
+
+def get_int(msg):
+    x = input(msg)
+    try:
+        return int(x)
+    except ValueError:
+        print("Error: not an integer")
+        return get_int(msg)
+
+
+def calculate(x, m, c):
+    y = (m * x) + c
+    print(f"Where x = {x}, y = {y}")
+
+
+m = get_int("Please enter an integer: ")
+c = get_int("Please enter another integer: ")
+
+
+for x in range(1, 11):
+    calculate(x, m, c)
+
+
+calculate(get_int("Please enter a value for x: "), m, c)
