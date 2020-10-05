5e452a069a88c1b418bb52b7ae1ffccbd0a561ad
Joey Sabey <joey.sabey@gmx.com>
2020-10-05 18:58:16 +0100

    Added alt.py to week2 - much cleaner way to achieve the same results

---
+++ week2/alt.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct  5 18:31:30 2020
+
+@author: Joey (100322897)
+"""
+
+
+# My student ID number as a list of digits
+id = [1,0,0,3,2,2,8,9,7]
+
+print("| 0 1 2 3 4 5 6 7 8 9")
+
+for x in id:
+    print("|-" + "--" * x + "X")
+
+print("| 0 1 2 3 4 5 6 7 8 9")
+
