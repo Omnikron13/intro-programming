2754deb2d92c1d60c8acda55e4f4c1c29c3b5a1e
Joey Sabey <joey.sabey@gmx.com>
2020-10-12 17:54:24 +0100

    Added some comments to the alt.py version of the week2 assessed lab work

--- week2/alt.py
+++ week2/alt.py
@@ -9,10 +9,13 @@ Created on Mon Oct  5 18:31:30 2020
 # My student ID number as a list of digits
 id = [1,0,0,3,2,2,8,9,7]
 
+# Print a header
 print("| 0 1 2 3 4 5 6 7 8 9")
 
+# Print bars indicating each number in the ID
 for x in id:
     print("|-" + "--" * x + "X")
 
+# Print a footer
 print("| 0 1 2 3 4 5 6 7 8 9")
 
