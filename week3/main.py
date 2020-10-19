70b728f0012a196414e5649b0d70b5f33aaf8d2b
Joey Sabey <joey.sabey@gmx.com>
2020-10-19 17:46:21 +0100

    Added main.py to week3 - contains linear() function for assessed tasks initially

---
+++ week3/main.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+@author: Joey (100322897)
+"""
+
+
+def linear(m, c):
+    """
+    Solves the equation y=mx+c for x where y=0 and m & c are arbitrary numbers.
+
+    :param m: arbitrary non-zero number
+    :param c: arbitrary number
+    :return: x
+    """
+    return (0-c)/m
